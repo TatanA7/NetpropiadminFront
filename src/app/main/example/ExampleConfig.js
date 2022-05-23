a7cca600ab50a62d146bffd7e350ef45327b3638
Javier Romero <tolomerog@gmail.com>
2022-05-23 10:46:17 -0500

    primer commit con componente register

--- src/app/main/example/ExampleConfig.js
+++ src/app/main/example/ExampleConfig.js
@@ -3,7 +3,8 @@ import i18next from 'i18next';
 import en from './i18n/en';
 import tr from './i18n/tr';
 import ar from './i18n/ar';
-import Example from './Example';
+// import Example from './Example';
+import SplitScreenSignUpPage from '../pages/authentication/sign-up/SplitScreenSignUpPage';
 
 i18next.addResourceBundle('en', 'examplePage', en);
 i18next.addResourceBundle('tr', 'examplePage', tr);
@@ -16,9 +17,13 @@ const ExampleConfig = {
     },
   },
   routes: [
+    // {
+    //   path: 'example',
+    //   element: <FullScreenSignInPage />,
+    // },
     {
       path: 'example',
-      element: <Example />,
+      element: <SplitScreenSignUpPage />,
     },
   ],
 };
