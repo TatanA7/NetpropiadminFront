58bb9aa3fb469dba0f83e8ae716fa24c715b1906
Ricardo <Ricardo@example.com>
2022-07-13 22:01:21 -0500

    Config graphql env

--- src/@gql-sdk/baseApi.ts
+++ src/@gql-sdk/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 import { GraphQLClient } from 'graphql-request'
 
-export const client = new GraphQLClient('http://localhost:3001/graphql')
+export const client = new GraphQLClient(process.env.REACT_APP_GRAPHQL_ENDPOINT)
 // highlight-start
 export const api = createApi({
     baseQuery: graphqlRequestBaseQuery({ client }),
